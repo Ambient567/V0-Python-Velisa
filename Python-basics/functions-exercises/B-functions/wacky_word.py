d92ba2d6c83bbc268681cf80ac0ef0dc3773abf2
Precious Nhlapho <pnhlapho59@gmail.com>
2026-01-14 16:02:46 +0200

    Add wacky word function

---
+++ Python-basics/functions-exercises/B-functions/wacky_word.py
@@ -0,0 +1,16 @@
+# Write a function `wacky_word` that accepts two strings.
+# Return a new string made from:
+# - the first 3 characters of the first string
+# - the last 2 characters of the second string
+#
+# The first string is at least 3 chars long.
+# The second string is at least 2 chars long.
+
+def wacky_word(word1, word2):
+    return word1[:3] + word2[-2:]
+
+print(wacky_word("very", "kindly"))       # "verly"
+print(wacky_word("forever", "sick"))      # "forck"
+print(wacky_word("cellar", "door"))       # "celor"
+print(wacky_word("bagel", "sweep"))       # "bagep"
+
