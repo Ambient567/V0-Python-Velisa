164c0d71d3534fbeafaf4482accd5d80792dfad6
Precious Nhlapho <pnhlapho59@gmail.com>
2026-01-20 20:42:55 +0200

    Add multiplication table function

---
+++ Python-basics/loops/A-loops/multiplication.py
@@ -0,0 +1,12 @@
+# Print the multiplication table of a number up to 10.
+
+def multiplication_table(num):
+    for x in range(11):
+        print(x * num)
+
+multiplication_table(4)
+# 4
+# 8
+# 12
+# ...
+# 40
