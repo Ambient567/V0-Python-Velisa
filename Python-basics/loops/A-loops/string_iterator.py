2e9928a8d73b60d1fcfa3bc17bc4a4c85963159b
Precious Nhlapho <pnhlapho59@gmail.com>
2026-01-20 16:24:09 +0200

    Add string iterator function

---
+++ Python-basics/loops/A-loops/string_iterator.py
@@ -0,0 +1,8 @@
+# Write a function `string_iterate(text)` that prints each character of the string.
+
+def string_iterate(str):
+    for ch in str:
+        print(ch)
+
+string_iterate("celery")
+string_iterate("hat")
