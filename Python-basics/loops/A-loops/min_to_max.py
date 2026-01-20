8c67da84583b3385b0f607cd99d2f49d00d49c79
Precious Nhlapho <pnhlapho59@gmail.com>
2026-01-20 16:13:24 +0200

    Add min max function

---
+++ Python-basics/loops/A-loops/min_to_max.py
@@ -0,0 +1,11 @@
+# Write a function `min_to_max(min_num, max_num)` that prints all numbers from min to max inclusive.
+
+
+def min_to_max(min_num, max_num):
+    for x in range(min_num, max_num+1):
+        print(x)
+
+min_to_max(5, 9)
+min_to_max(11, 13)
+min_to_max(20, 11)   # what happens here?
+
