099de359961c0dcb1ff22dc2534b70a1e21abc23
Precious Nhlapho <pnhlapho59@gmail.com>
2026-01-21 14:31:05 +0200

    Add five multiples of function

---
+++ Python-basics/loops/B-loops/five_multiplies.py
@@ -0,0 +1,16 @@
+# Write a function `five_multiples_of(n)` that prints the first five multiples of n.
+# The function does not return a value, just prints.
+
+# Example:
+
+def five_multiples_of(num):
+    for x in range(1, 6):
+        print(x * num)
+
+five_multiples_of(7)
+
+# 7
+# 14
+# 21
+# 28
+# 35
