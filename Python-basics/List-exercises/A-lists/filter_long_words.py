0fc39481fb570a4818854f0e23c1b22cdf1a530a
Precious Nhlapho <pnhlapho59@gmail.com>
2026-01-22 22:11:18 +0200

    Add filter long words function

---
+++ Python-basics/List-exercises/A-lists/filter_long_words.py
@@ -0,0 +1,17 @@
+# Write a function `filter_long_words(words)` that accepts a list of strings.
+# The function should return a new list containing only the words that have
+# less than 5 characters.
+
+def filter_long_words(words):
+    new_list = []
+
+    for word in range(len(words)):
+        if len(words[word]) < 5:
+            new_list.append(words[word])
+
+    print(new_list)
+
+# Example:
+filter_long_words(["kale", "cat", "retro", "axe", "heirloom"]) #-> ['kale', 'cat', 'axe']
+filter_long_words(["disrupt", "pour", "trade", "pic"]) #-> ['pour', 'pic']
+
