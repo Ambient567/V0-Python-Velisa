9de8b9ce7738bda380b3c5c07a0648168741f1de
Precious Nhlapho <pnhlapho59@gmail.com>
2026-02-10 14:41:07 +0200

    Add pluck function

---
+++ Python-basics/dictionaries/C-dictionary/pluck.py
@@ -0,0 +1,25 @@
+# Write a function `pluck` that accepts:
+# - a dictionary
+# - a list of strings
+# The function should return a new dictionary containing only the key–value pairs
+# where the key exists in the provided list.
+
+def pluck(d, keys):
+    result = {}
+    for key in keys:
+        if key in d:
+            result[key] = d[key]
+    return result
+
+
+print(pluck(
+    {"name":"Fido","color":"Brown","breed":"German Shepherd"},
+    ["name","breed"]
+))
+# { "name": "Fido", "breed": "German Shepherd" }
+
+print(pluck(
+    {"make":"Tesla","mpg":93,"model":"Model X","color":"white"},
+    ["make","model"]
+))
+# { "make": "Tesla", "model": "Model X" }
