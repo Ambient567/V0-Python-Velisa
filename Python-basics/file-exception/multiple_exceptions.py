f36a220defac1d451bd8176cea15ece1c01b6465
Precious Nhlapho <pnhlapho59@gmail.com>
2026-02-17 21:59:21 +0200

    Catching Multiple Exceptions

---
+++ Python-basics/file-exception/multiple_exceptions.py
@@ -0,0 +1,11 @@
+try:
+    x = int(input("Enter a number: "))
+    print(10 / x)
+
+except ZeroDivisionError:
+    print("You cannot divide by zero")
+
+except ValueError:
+    print("Please enter a valid number")
+
+
